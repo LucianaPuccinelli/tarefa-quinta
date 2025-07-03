c4242bbfff0309bc48051b8d97dcf7b3b4e79590
LucianaPuccinelli <ll22puccinelli@gmail.com>
2025-07-03 01:48:01 -0300

    removendo agora de novo e removendo porta

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -1,7 +1,7 @@
 <?php echo $this->Html->css('styles.css'); ?>
 
 <div class="container">
-    <h1>Escolha o que deseja fazer AGORA</h1>
+    <h1>Escolha o que deseja fazer</h1>
     <div class="button-group">
         <a href="<?= $this->Url->build(['controller' => 'Usuario', 'action' => 'index']); ?>" class="btn"><i class="fa-solid fa-user"></i> Usuários</a>
         <a href="<?= $this->Url->build(['controller' => 'Tarefa', 'action' => 'index']); ?>" class="btn"><i class="fa-solid fa-tag"></i> Tarefas</a>
